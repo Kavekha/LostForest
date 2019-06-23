ef5b454a676ca2d5c8bc185cdfe73a149193556f
Kavekha <40246978+Kavekha@users.noreply.github.com>
2019-06-23 02:02:10 +0200

    0.4.2 New char screen & translations.
        Fiche de personnage contient les nouvelles statistiques.
        Traduction des termes de jeu, de la fiche de personnage et de la fin standard.
        Gain d'HP max au leveling.

--- docs/combat_and_stats.md
+++ docs/combat_and_stats.md
@@ -1,26 +1,14 @@
 # Stats
 
-HP      :   Points de vie.
-Might   :   Puissance des dommages. Limite la puissance des coups si trop faible, augmente les dommages si trop fort.
-Vitality :  Reduit les dmg subits. Si dommages trop faibles, ils sont réduits. Si trop forts, ils sont augmentés.
-
-# Combat flow
-
-Si les deux entités ont un component Fighter & block True, le combat a lieu à l'interaction.
-Attaquant .attack pour X damage (Base dmg * Might).
-Defenseur .get hit. Il modifie les dmg et retourne leur nouvelle valeur.
-Defenseur .take_damage. Verifie si le defenseur meurt.
-Attaquant .kill si Defenseur meurt.
-Defenseur .on_death si Defenseur meurt.
+HP :        Points de vie.
+MIGHT :     Puissance physique.
+VITALITY :  Resistance naturelle, constitution, vitalité.
+DEXTERITE : Chances de toucher, chances d'esquive, precision des coups.
 
 
-# Damage formula
+# Formule de dommages, physique.
 
-Si damage superieur à Might, limité à might.
-Si damage inferieur à Might x 2, damage doublés.
-Sinon, damage de base.
-i.e :
-J'ai Might 5. J'utilise une arme 2-4. Je fais 2. J'inflige 4 dmg. Vrais Dmg : 4, 3, 4.
-J'ai Might 5. J'utilise une arme 3-5. Je fais 3. J'inflige 3 dmg. Vrais dmg : 3, 4, 5.
-J'ai Might 1. J'utilise une arme 2-4. Je fais 2. J'inflige 1 dmg. Vrais dmg : 1, 1, 1.
+1. On calcule puissance physique.
+    Chaque stat augmente la valeur de puissance physique, selon un ratio favorable à Might.
+    On ajoute aussi le bonus dommages de l'arme, si elle en a.
 
--- menus/level_menu.py
+++ menus/level_menu.py
@@ -1,5 +1,6 @@
 from menus.menu import Menu
 from systems.localization import Texts
+from config import game_config
 
 
 class LevelUpMenu(Menu):
@@ -12,19 +13,25 @@ class LevelUpMenu(Menu):
         self.header = Texts.get_text('LEVEL_UP_MENU_HEADER').format(
             self.source.level.available_stat_points
         )
-        self.display_options = []
-        self._options = ["Might", "Vitality", "HP"]
+        self.display_options = [Texts.get_text('MIGHT_GAME_TERM'),
+                                Texts.get_text('DEXTERITY_GAME_TERM'),
+                                Texts.get_text('VITALITY_GAME_TERM')
+                                ]
+        self._options = ["Might", 'Dexterity', "Vitality"]
 
     def return_choice_result(self, string_choice):
         string_choice = string_choice.lower()
         if string_choice == "might":
             self.source.fighter.base_might += 1
+        elif string_choice == "dexterity":
+            self.source.fighter.base_dexterity += 1
         elif string_choice == "vitality":
             self.source.fighter.base_vitality += 1
-        elif string_choice == "hp":
-            self.source.fighter.base_max_hp += 5
-            self.source.fighter.hp += 5
 
+        # on augmente aussi HP Max.
+        hp_gain = 1 * game_config.HP_GAIN_AT_LEVEL__UP
+        self.source.fighter.base_max_hp += hp_gain
+        self.source.fighter.hp += hp_gain
         self.source.level.available_stat_points -= 1
         if self.source.level.available_stat_points > 0:
             self.update_options()
